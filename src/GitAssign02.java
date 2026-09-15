68ffd268c1ec8b72b442f8ad16347c1f5d73753a
ocean_doggie <kerzhang1@gmail.com>
2026-09-15 12:35:29 -0400

    Added Keren line GitAssign02

--- src/GitAssign02.java
+++ src/GitAssign02.java
@@ -1,5 +1,6 @@
 public class GitAssign02 {
     public static void main(String[] args) {
         System.out.println("Hello, Sabina!");
+        System.out.println("This is Keren who participated in the Git assignment.");
     }
 }
