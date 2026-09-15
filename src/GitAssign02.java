2a75bb773ad4045d708810ade7239874e66fe868
Sabina Nuriyeva <sabina.n5111@gmail.com>
2026-09-15 12:42:18 -0400

    Sabina added message

--- src/GitAssign02.java
+++ src/GitAssign02.java
@@ -2,5 +2,6 @@ public class GitAssign02 {
     public static void main(String[] args) {
         System.out.println("Hello, Sabina!");
         System.out.println("This is Keren who participated in the Git assignment.");
+        System.out.println("Hello, McKenzie!");
     }
 }
